6888532bc28de7ecc883034792dcecab770f9cc5
gusdn <gusdn@NaeMoo>
2020-03-04 23:00:19 +0900

    채팅방 클라이언트 접속 해제 및 서버 중지 추가

--- src/ChatClient/Client.java
+++ src/ChatClient/Client.java
@@ -23,17 +23,17 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
-public class Client extends JFrame implements ActionListener{
+public class Client extends JFrame implements ActionListener {
 
-	//Login GUI resource
+	// Login GUI resource
 	private JFrame login_GUI = new JFrame();
 	private JPanel login_pane;
 	private JTextField ip_tf;
 	private JTextField port_tf;
 	private JTextField id_tf;
 	private JButton login_btn = new JButton("접속");
-	
-	//Main을 GUI resource
+
+	// Main을 GUI resource
 	private JPanel contentPane;
 	private JTextField chat_tf;
 	private JButton note_btn = new JButton("쪽지 보내기");
@@ -44,116 +44,117 @@ public class Client extends JFrame implements ActionListener{
 	private JList user_list = new JList();
 	private JList room_list = new JList();
 
-	//Login resource
+	// Login resource
 	private String ip;
 	private int port;
 	private String id;
-	
-	//Network resource
+
+	// Network resource
 	private Socket sock;
 	private InputStream is;
 	private OutputStream os;
 	private DataInputStream dis;
 	private DataOutputStream dos;
-	
-	//the others 
-	private String msg;//메세지
-	private Vector<String> user_vector = new Vector<>();//user_list에 추가 위해
-	private Vector<String> room_vector = new Vector<>();//room_list에 추가 위해
-	private StringTokenizer st;//inMessage 함수에서 사용하기 위한 파싱 변수
+
+	// the others
+	private String msg;// 메세지
+	private Vector<String> user_vector = new Vector<>();// user_list에 추가 위해
+	private Vector<String> room_vector = new Vector<>();// room_list에 추가 위해
+	private StringTokenizer st;// inMessage 함수에서 사용하기 위한 파싱 변수
 	private String myRoom = null;
-	
+
 	public Client() {
 		loginInit();
 		mainInit();
 		start();
 	}
-	
-	private void loginInit() {//Login GUI구성
+
+	private void loginInit() {// Login GUI구성
 		login_GUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		login_GUI.setBounds(100, 100, 310, 397);
 		login_pane = new JPanel();
 		login_pane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		login_GUI.setContentPane(login_pane);
 		login_pane.setLayout(null);
-		
+
 		ip_tf = new JTextField();
 		ip_tf.setBounds(103, 186, 149, 21);
 		login_pane.add(ip_tf);
 		ip_tf.setColumns(10);
-		
+
 		port_tf = new JTextField();
 		port_tf.setBounds(103, 227, 149, 21);
 		login_pane.add(port_tf);
 		port_tf.setColumns(10);
-		
+
 		id_tf = new JTextField();
 		id_tf.setBounds(103, 274, 149, 21);
 		login_pane.add(id_tf);
 		id_tf.setColumns(10);
-		
+
 		JLabel lblNewLabel = new JLabel("Servr IP");
 		lblNewLabel.setBounds(22, 189, 62, 15);
 		login_pane.add(lblNewLabel);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Server Port");
 		lblNewLabel_1.setBounds(22, 230, 69, 15);
 		login_pane.add(lblNewLabel_1);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("ID");
 		lblNewLabel_2.setBounds(41, 277, 23, 15);
 		login_pane.add(lblNewLabel_2);
-		
+
 		login_btn.setBounds(71, 314, 153, 23);
 		login_pane.add(login_btn);
-		
+
 		login_GUI.setVisible(true);
 	}
-	
-	private void mainInit() {//Main GUI 구성
+
+	private void mainInit() {// Main GUI 구성
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 629, 459);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("전체 접속자");
 		lblNewLabel.setBounds(12, 10, 105, 24);
 		contentPane.add(lblNewLabel);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("채팅방 목록");
 		lblNewLabel_1.setBounds(12, 213, 83, 15);
 		contentPane.add(lblNewLabel_1);
-		
+
 		note_btn.setBounds(26, 180, 101, 23);
 		contentPane.add(note_btn);
-		
+
 		join_btn.setBounds(22, 350, 111, 23);
 		contentPane.add(join_btn);
-		
+
 		create_btn.setBounds(26, 383, 107, 23);
 		contentPane.add(create_btn);
-		
+
 		chat_area.setBounds(141, 10, 462, 363);
 		contentPane.add(chat_area);
-		
+		chat_area.setEditable(false);
+
 		chat_tf = new JTextField();
 		chat_tf.setBounds(145, 384, 363, 21);
 		contentPane.add(chat_tf);
 		chat_tf.setColumns(10);
-		
+
 		send_btn.setBounds(520, 383, 83, 23);
 		contentPane.add(send_btn);
-		
+
 		user_list.setBounds(22, 44, 105, 123);
 		contentPane.add(user_list);
-		
+
 		room_list.setBounds(22, 240, 105, 100);
 		contentPane.add(room_list);
 	}
-	
-	private void start() {//메세지 시작
+
+	private void start() {// 메세지 시작
 		login_btn.addActionListener(this);
 		note_btn.addActionListener(this);
 		join_btn.addActionListener(this);
@@ -161,20 +162,21 @@ public class Client extends JFrame implements ActionListener{
 		send_btn.addActionListener(this);
 	}
 
-	private void network() {//network 자원 구성 -> 쓰레드를 이용
+	private void network() {// network 자원 구성 -> 쓰레드를 이용
 		try {
 			sock = new Socket(ip, port);
 		} catch (UnknownHostException e) {
-			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "서버를 찾지 못 하였습니다.","연결 실패", JOptionPane.ERROR_MESSAGE);
 		} catch (IOException e) {
-			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "서버와 연결 실패","연결 실패", JOptionPane.ERROR_MESSAGE);
 		}
-		if(sock!=null)//성공적으로 연결 되었다면 그 다음 과정 실행
+		if (sock != null)// 성공적으로 연결 되었다면 그 다음 과정 실행
 			connection();
-		
+
 	}
-	private void connection() {//연결 시 행동
-		try {//1.입출력 스트림 설정 
+
+	private void connection() {// 연결 시 행동
+		try {// 1.입출력 스트림 설정
 			is = sock.getInputStream();
 			dis = new DataInputStream(is);
 			os = sock.getOutputStream();
@@ -182,17 +184,26 @@ public class Client extends JFrame implements ActionListener{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		//2.현재 접속자에 자신을 추가한다.
+		// 2.현재 접속자에 자신을 추가한다.
 		sendMessage(id);
 		user_vector.add(id);
-		
-		//3. 서버 소켓과 통신 시작
-		Thread th = new Thread(()-> {
-			while(true) {
+
+		// 3. 서버 소켓과 통신 시작
+		Thread th = new Thread(() -> {
+			while (true) {
 				try {
 					msg = dis.readUTF();
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
+					try {
+						is.close();
+						dis.close();
+						os.close();
+						dos.close();
+						JOptionPane.showMessageDialog(null, "서버와의 접속이 끊어졌습니다.", "서버와 연결 실패", JOptionPane.ERROR_MESSAGE);
+						break;
+					} catch (IOException e1) {
+
+					}
 					e.printStackTrace();
 				}
 				inMessage(msg);
@@ -200,7 +211,7 @@ public class Client extends JFrame implements ActionListener{
 		});
 		th.start();
 	}
-	
+
 	private void sendMessage(String str) {
 		try {
 			dos.writeUTF(str);
@@ -209,123 +220,122 @@ public class Client extends JFrame implements ActionListener{
 			e.printStackTrace();
 		}
 	}
-	
-	private void inMessage(String str) {//protocol에 따라 메세지 처리 -> 파싱을 위해
-		st = new StringTokenizer(str,"/");
+
+	private void inMessage(String str) {// protocol에 따라 메세지 처리 -> 파싱을 위해
+		st = new StringTokenizer(str, "/");
 		String protocol = st.nextToken();
 		String Message = st.nextToken();
-		
-		if(protocol.equals("NewUser")){//새로운 유저가 들어왔을 시 
+
+		if (protocol.equals("NewUser")) {// 새로운 유저가 들어왔을 시
 			user_vector.add(Message);
 			Collections.sort(user_vector);
-		}
-		else if(protocol.equals("OldUser")) {
+		} else if (protocol.equals("OldUser")) {
 			user_vector.add(Message);
 			Collections.sort(user_vector);
-		}
-		else if(protocol.equals("Note")) {
+		} else if (protocol.equals("Note")) {
 			String user = Message;
 			String content = st.nextToken();
-			JOptionPane.showMessageDialog(null, content, user+"로 부터 온 쪽지", JOptionPane.PLAIN_MESSAGE);
-		}
-		else if(protocol.equals("user_vector_update")) {//user_list 갱신 프로토콜
+			JOptionPane.showMessageDialog(null, content, user + "로 부터 온 쪽지", JOptionPane.PLAIN_MESSAGE);
+		} else if (protocol.equals("user_vector_update")) {// user_list 갱신 프로토콜
 			user_list.setListData(user_vector);
-		}
-		else if(protocol.equals("CreteRoomFail")) {
+		} else if (protocol.equals("CreteRoomFail")) {
 			JOptionPane.showMessageDialog(null, "이미 존재하는 방 이름입니다.", "방 생성 실패", JOptionPane.ERROR_MESSAGE);
-		}
-		else if(protocol.equals("NewRoom")) {
+		} else if (protocol.equals("NewRoom")) {
 			room_vector.add(Message);
 			Collections.sort(room_vector);
 			room_list.setListData(room_vector);
-		}
-		else if(protocol.equals("CreateRoom")) {
+		} else if (protocol.equals("CreateRoom")) {
 			myRoom = Message;
-		}
-		else if(protocol.equals("Chat")) {
+			create_btn.setEnabled(false);
+			join_btn.setEnabled(false);
+		} else if (protocol.equals("Chat")) {
 			String user = Message;
 			String cont = st.nextToken();
-			chat_area.append(user+": "+cont);
-		}
-		else if(protocol.equals("OriginalRoom")) {
+			chat_area.append(user + ": " + cont);
+		} else if (protocol.equals("OriginalRoom")) {
 			room_vector.add(Message);
-		}
-		else if(protocol.equals("room_vector_update")) {
+		} else if (protocol.equals("room_vector_update")) {
 			room_list.setListData(room_vector);
-		}
-		else if(protocol.equals("AccessRoom")) {
+		} else if (protocol.equals("AccessRoom")) {
 			myRoom = Message;
-			JOptionPane.showMessageDialog(null, Message+"에 참가합니다.", "방 참가", JOptionPane.PLAIN_MESSAGE);
-		}
-		else if(protocol.equals("NewRoomUser")) {
-			chat_area.append(Message+"님이 참가하셨습니다.\n");
+			create_btn.setEnabled(false);
+			join_btn.setEnabled(false);
+			JOptionPane.showMessageDialog(null, Message + "에 참가합니다.", "방 참가", JOptionPane.PLAIN_MESSAGE);
+		} else if (protocol.equals("NewRoomUser")) {
+			chat_area.append(Message + "님이 참가하셨습니다.\n");
+		} else if (protocol.equals("UserOut")) {
+			int idx = Collections.binarySearch(user_vector, Message);
+			user_vector.remove(idx);
+			Collections.sort(user_vector);
+		} else if (protocol.equals("EmptyRoom")) {
+			int idx = Collections.binarySearch(room_vector, Message);
+			room_vector.remove(idx);
 		}
 	}
-	
+
 	@Override
-	public void actionPerformed(ActionEvent e) {//핸들러 처리
-		if(e.getSource() == login_btn) {
+	public void actionPerformed(ActionEvent e) {// 핸들러 처리
+		if (e.getSource() == login_btn) {
 			try {
 				ip = ip_tf.getText().trim();
 				port = Integer.parseInt(port_tf.getText().trim());
 				id = id_tf.getText().trim();
 				network();
-			}
-			catch(Exception er) {
+				login_GUI.setVisible(false);
+				this.setVisible(true);
+			} catch (Exception er) {
 				System.out.println("서버 정보입력 실패");
 			}
-			login_GUI.setVisible(false);
-			this.setVisible(true);
 		}
-		if(e.getSource()==note_btn) {
-			String user = (String)user_list.getSelectedValue();
-			if(user !=null) {
+		if (e.getSource() == note_btn) {
+			String user = (String) user_list.getSelectedValue();
+			if (user != null) {
 				msg = JOptionPane.showInputDialog("보낼 쪽지 내용을 적어주세요.");
-				//protocol -> Note/보낼 사람/쪽지내용
-				if(msg!=null) {
+				// protocol -> Note/보낼 사람/쪽지내용
+				if (msg != null) {
 					msg = "Note/" + user + "/" + msg;
 					sendMessage(msg);
 				}
-			}
-			else {
-				JOptionPane.showMessageDialog(null, "쪽지를 보낼 상대를 선택해주세요.", "쪽지 보내기 오류",JOptionPane.ERROR_MESSAGE);
+			} else {
+				JOptionPane.showMessageDialog(null, "쪽지를 보낼 상대를 선택해주세요.", "쪽지 보내기 오류", JOptionPane.ERROR_MESSAGE);
 			}
 		}
-		if(e.getSource()==join_btn) {
-			String roomName = (String)room_list.getSelectedValue();
-			if(roomName!=null) {
+		if (e.getSource() == join_btn) {
+			String roomName = (String) room_list.getSelectedValue();
+			if (roomName != null) {
 				msg = "JoinRoom/" + roomName;
 				sendMessage(msg);
-			}
-			else {
-				JOptionPane.showMessageDialog(null, "들어갈 방을 선택해주세요.", "방 참가 오류",JOptionPane.ERROR_MESSAGE);
+			} else {
+				JOptionPane.showMessageDialog(null, "들어갈 방을 선택해주세요.", "방 참가 오류", JOptionPane.ERROR_MESSAGE);
 			}
 		}
-		if(e.getSource()==create_btn) {//채팅방 생성 protocol - CreateRoom/roomName
+		if (e.getSource() == create_btn) {// 채팅방 생성 protocol - CreateRoom/roomName
 			msg = JOptionPane.showInputDialog("채팅방 이름을 설정하세요.");
-			if(msg != null) {
-				if(msg.equals("")) {
+			if (msg != null) {
+				if (msg.equals("")) {
 					JOptionPane.showMessageDialog(null, "방 이름을 설정해 주세요.", "방 생성 실패", JOptionPane.ERROR_MESSAGE);
-				}
-				else {
+				} else {
 					msg = "CreateRoom/" + msg;
 					sendMessage(msg);
 				}
 			}
 		}
-		if(e.getSource()==send_btn) {
+		if (e.getSource() == send_btn) {
 			msg = chat_tf.getText().trim();
-			//대화 전송 프로토콜 -Chat/속한 방이름/내용
-			if(msg!=null) {
+			// 대화 전송 프로토콜 -Chat/속한 방이름/내용
+			if (myRoom == null) {
+				JOptionPane.showMessageDialog(null, "속해있는 방이 없습니다.", "전송 실패", JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+			if (msg != null) {
 				msg = "Chat/" + myRoom + "/" + msg;
 				chat_tf.setText("");
 				sendMessage(msg);
 			}
 		}
-		
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		new Client();
 	}
--- src/ChatServer/Server.java
+++ src/ChatServer/Server.java
@@ -10,144 +10,161 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-import javax.print.attribute.standard.Severity;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
-public class Server extends JFrame implements ActionListener{
-	
-	//GUI resource
+public class Server extends JFrame implements ActionListener {
+
+	// GUI resource
 	private JPanel contentPane;
 	private JTextField port_tf;
 	private JTextArea textArea = new JTextArea();
 	private JButton start_btn = new JButton("서버 실행");
 	private JButton stop_btn = new JButton("서버 중단");
-	
-	//Network resource
+
+	// Network resource
 	private int port;
 	private ServerSocket server_sock;
 	private Socket sock;
-	
-	//others
-	private List<UserInfo> user_list = new LinkedList<>();//클라이언트 정보 저장
+
+	// others
+	private List<UserInfo> user_list = new LinkedList<>();// 클라이언트 정보 저장
 	private List<RoomInfo> room_list = new LinkedList<>();
 	private StringTokenizer st;
-	
-	
-	public Server() {//생성자 
+
+	public Server() {// 생성자
 		init();
 		startListen();
 	}
-	private void init() {//Server GUI 생성
+
+	private void init() {// Server GUI 생성
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 408, 453);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		textArea.setBounds(12, 10, 366, 285);
 		contentPane.add(textArea);
-		
+		textArea.setEditable(false);
+
 		JLabel lblNewLabel = new JLabel("포트 번호 :");
 		lblNewLabel.setBounds(28, 317, 71, 15);
 		contentPane.add(lblNewLabel);
-		
+
 		port_tf = new JTextField();
 		port_tf.setBounds(111, 314, 267, 21);
 		contentPane.add(port_tf);
 		port_tf.setColumns(10);
-		
+
 		start_btn.setBounds(28, 357, 166, 23);
 		contentPane.add(start_btn);
-		
+
 		stop_btn.setBounds(206, 357, 172, 23);
 		contentPane.add(stop_btn);
-		
+
 		setVisible(true);
 	}
-	
-	private void startListen() {//컴포넌트 메세지 처리
+
+	private void startListen() {// 컴포넌트 메세지 처리
 		start_btn.addActionListener(this);
 		stop_btn.addActionListener(this);
 	}
-	
+
 	@Override
-	public void actionPerformed(ActionEvent e) {//메세지 처리 함수
-		if(e.getSource() == start_btn) {
-			System.out.println("서버 실행 버튼");
+	public void actionPerformed(ActionEvent e) {// 메세지 처리 함수
+		if (e.getSource() == start_btn) {
+			start_btn.setEnabled(false);
+			stop_btn.setEnabled(true);
+			port_tf.setEnabled(false);
 			initListenSock();
 			connetion();
 		}
-		if(e.getSource() == stop_btn) {
+		if (e.getSource() == stop_btn) {
+			try {
+				if(sock != null)
+					sock.close();
+				if(server_sock !=null)
+					server_sock.close();
+				user_list.clear();
+				room_list.clear();
+				st = null;
+				sock = null;
+				server_sock = null;
+				
+				start_btn.setEnabled(true);
+				stop_btn.setEnabled(false);
+				port_tf.setEnabled(true);
+				
+			} catch (IOException e1) {
+			}
 			System.out.println("서버 중단 버튼");
 		}
 	}
-	
-	private void initListenSock() {//Listen 소켓 초기화
+
+	private void initListenSock() {// Listen 소켓 초기화
 		try {
 			port = Integer.parseInt(port_tf.getText().trim());
 			server_sock = new ServerSocket(port);
-		}
-		catch(NumberFormatException e) {
+		} catch (NumberFormatException e) {
 			System.out.println("잘못된 port 입력");
 			e.printStackTrace();
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			System.out.println("서버 소켓 생성 실패");
 			e.printStackTrace();
 		}
 	}
 
-	private void connetion() {//대기 큐에 있는 소켓 연결 - Thread 사용으로  Blocking 방지
-		Thread th = new Thread(()-> {
-			while(true) {
+	private void connetion() {// 대기 큐에 있는 소켓 연결 - Thread 사용으로 Blocking 방지
+		Thread th = new Thread(() -> {
+			while (true) {
 				try {
 					textArea.append("사용자와 연결 대기 중\n");
 					sock = server_sock.accept();
 					textArea.append("사용자와 연결 성공\n");
-					
+
 					UserInfo user = new UserInfo(sock);
 					user.start();
-					
+
 					user_list.add(user);
-					broadCast("user_vector_update/ ");//user_list 갱신 protocol
-					broadCast("room_vector_update/ ");//user_list 갱신 protocol
+					broadCast("user_vector_update/ ");// user_list 갱신 protocol
+					broadCast("room_vector_update/ ");// user_list 갱신 protocol
 				} catch (IOException e) {
-					System.out.println("클라이언트와의 연결 실패");
-					e.printStackTrace();
+					break;
 				}
 			}
 		});
 		th.start();
 	}
-	
-	private void broadCast(String str) {//모든 클라이언트에게 메세지 보내기
+
+	private void broadCast(String str) {// 모든 클라이언트에게 메세지 보내기
 		for (int i = 0; i < user_list.size(); i++) {
 			user_list.get(i).sendMessage(str);
 		}
 	}
-	
-	class UserInfo extends Thread{
+
+	class UserInfo extends Thread implements Comparable<String>{
 		private InputStream is;
 		private DataInputStream dis;
 		private OutputStream os;
 		private DataOutputStream dos;
-		
+
 		private String NickName;
+		private String myRoom = null;
 		private Socket user_sock;
 		private String msg;
-		
+
 		UserInfo(Socket sock) {
 			this.user_sock = sock;
 			try {
@@ -155,129 +172,165 @@ public class Server extends JFrame implements ActionListener{
 				dis = new DataInputStream(is);
 				os = sock.getOutputStream();
 				dos = new DataOutputStream(os);
-				
-				NickName = dis.readUTF();
-				textArea.append(NickName +"님이 입장하셨습니다.\n");
 
-				msg = "NewUser/"+NickName;
-				
-			
+				NickName = dis.readUTF();
+				textArea.append(NickName + "님이 입장하셨습니다.\n");
+
+				msg = "NewUser/" + NickName;
+
 				synchronized (user_list) {
-					//기존 유저에게 새로운 유저 전달
+					// 기존 유저에게 새로운 유저 전달
 					broadCast(msg);
-					
+
 					// 새로운 유저에게 기존 유저 전달
 					for (int i = 0; i < user_list.size(); i++) {
-						msg = "OldUser/"+ user_list.get(i).NickName;
+						msg = "OldUser/" + user_list.get(i).NickName;
 						sendMessage(msg);
 					}
-					
+
 					// 새로운 유저에게 방 리스트 주기
-					for(int i = 0 ; i< room_list.size();i++) {
-						msg = "OriginalRoom/"+room_list.get(i).roomName;
+					for (int i = 0; i < room_list.size(); i++) {
+						msg = "OriginalRoom/" + room_list.get(i).roomName;
 						sendMessage(msg);
 					}
-					
+
 				}
-			}
-			catch(IOException e) {
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 		@Override
 		public void run() {
-			while(true) {
+			while (true) {
 				try {
 					msg = dis.readUTF();
 					inMessage(msg);
-				}
-				catch(IOException e) {
+				} catch (IOException e) {
+					try {
+						is.close();
+						dis.close();
+						os.close();
+						dos.close();
+						
+						//유저 삭제 됬다고 알리기
+						int idx = Collections.binarySearch(user_list, NickName);
+						user_list.remove(idx);
+						broadCast("UserOut/"+NickName);
+						
+						//방 관리
+						int room_idx = Collections.binarySearch(room_list, myRoom);
+						if(room_idx >= 0) {
+							RoomInfo r = room_list.get(room_idx);
+							idx = Collections.binarySearch(r.room_user_list, NickName);
+							if (idx >= 0) {
+								r.room_user_list.remove(idx);// 방 목록에서 나를 삭제한다.
+								if (r.room_user_list.size() == 0) {
+									broadCast("EmptyRoom/" + myRoom);
+									room_list.remove(room_idx);
+								}
+							}
+						}
+						
+						broadCast("user_vector_update/ ");
+						broadCast("room_vector_update/ ");
+						
+						JOptionPane.showMessageDialog(null, NickName+"와의 접속이 끊어졌습니다.", "사용자와 접속 해제", JOptionPane.ERROR_MESSAGE);
+						break;
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
 					e.printStackTrace();
 				}
 			}
 		}
+
 		private void inMessage(String str) {
-			st = new StringTokenizer(str,"/");
+			st = new StringTokenizer(str, "/");
 			String protocol = st.nextToken();
 			String Message = st.nextToken();
-			
-			if(protocol.equals("Chat")) {
+
+			if (protocol.equals("Chat")) {
 				String room = Message;
 				int roomIdx = Collections.binarySearch(room_list, room);
 				Message = st.nextToken();
-				if(roomIdx>=0) {//방이 존재할 때 해당 방에게 쪽지 돌림
-					Message = "Chat/"+NickName+"/"+Message+"\n";
+				if (roomIdx >= 0) {// 방이 존재할 때 해당 방에게 쪽지 돌림
+					Message = "Chat/" + NickName + "/" + Message + "\n";
 					room_list.get(roomIdx).roomBroadCast(Message);
 				}
-			}
-			else if(protocol.equals("Note")) {
+			} else if (protocol.equals("Note")) {
 				String user = Message;
 				String note = st.nextToken();
 				System.out.println(note);
-				for(int i=0;i<user_list.size();i++) {
-					if(user.equals(user_list.get(i).NickName)) {
-						note = "Note/"+NickName+"/"+note;
+				for (int i = 0; i < user_list.size(); i++) {
+					if (user.equals(user_list.get(i).NickName)) {
+						note = "Note/" + NickName + "/" + note;
 						user_list.get(i).sendMessage(note);
 					}
 				}
-			}
-			else if(protocol.equals("CreateRoom")) {
-				if(Collections.binarySearch(room_list, Message) < 0) {//겹치는 방 이름이 없을 경우
-					RoomInfo r = new RoomInfo(Message,this);//현재 클라이언트를 저장
+			} else if (protocol.equals("CreateRoom")) {
+				if (Collections.binarySearch(room_list, Message) < 0) {// 겹치는 방 이름이 없을 경우
+					RoomInfo r = new RoomInfo(Message, this);// 현재 클라이언트를 저장
 					room_list.add(r);
-					Collections.sort(room_list,(r1,r2)->r1.roomName.compareTo(r2.roomName));//알파벳순으로 정렬
-					//protocol - NewRoom/방이름
-					String newMessage = "NewRoom/"+Message;
+					myRoom = Message;
+					Collections.sort(room_list, (r1, r2) -> r1.roomName.compareTo(r2.roomName));// 알파벳순으로 정렬
+					// protocol - NewRoom/방이름
+					String newMessage = "NewRoom/" + Message;
 					broadCast(newMessage);
-					newMessage = "CreateRoom/"+Message;
+					newMessage = "CreateRoom/" + Message;
 					sendMessage(newMessage);
-				}
-				else {
+				} else {
 					sendMessage("CreteRoomFail/ ");
 				}
-			}
-			else if(protocol.equals("JoinRoom")) {
+			} else if (protocol.equals("JoinRoom")) {
 				int idx = Collections.binarySearch(room_list, Message);
-				room_list.get(idx).roomBroadCast("NewRoomUser/"+NickName);
+				room_list.get(idx).roomBroadCast("NewRoomUser/" + NickName);
 				room_list.get(idx).room_user_list.add(this);
-				msg = "AccessRoom/"+Message;//AccessRoom/방이름
+				myRoom = Message;
+				msg = "AccessRoom/" + Message;// AccessRoom/방이름
 				sendMessage(msg);
 			}
 		}
-		
-		private void sendMessage(String str) {//자신과 연결된 클라이언트에게 메세지 보내기
+
+		private void sendMessage(String str) {// 자신과 연결된 클라이언트에게 메세지 보내기
 			try {
 				dos.writeUTF(str);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
+		
+		@Override
+		public int compareTo(String o) {
+			return NickName.compareTo(o);
+		}
 	}
-	class RoomInfo implements Comparable<String>{
+
+	class RoomInfo implements Comparable<String> {
 
 		private String roomName;
 		private List<UserInfo> room_user_list = new LinkedList<>();
-		public RoomInfo(String roomName,UserInfo u) {
+
+		public RoomInfo(String roomName, UserInfo u) {
 			this.roomName = roomName;
 			room_user_list.add(u);
 		}
+
 		@Override
 		public int compareTo(String o) {
 			return roomName.compareTo(o);
 		}
-		
+
 		private void roomBroadCast(String str) {
-			for(int i = 0 ; i <room_user_list.size();i++) {
-				UserInfo  u = room_user_list.get(i);
+			for (int i = 0; i < room_user_list.size(); i++) {
+				UserInfo u = room_user_list.get(i);
 				u.sendMessage(str);
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		new Server();
 	}
 
 }
-
